4a2e78c7c11ae9106d95f5f46a40458263827a0e
Anselm Bradford <git@anselmbradford.com>
2014-03-14 18:20:35 -0400

    Removes fittext vendor script
    Fittext vendor script was not being used, so this commit removes its
    dependencies.

--- src/js/vendor/requireconfig.js
+++ src/js/vendor/requireconfig.js
@@ -2,7 +2,6 @@ requirejs.config({
 		baseUrl: 'js',
     paths: {
     		jquery: 'vendor/jquery-min',
-    		fittext: 'vendor/fittext',
         domReady: 'vendor/requirejs/domReady',
         async: 'vendor/requirejs/async'
     },
